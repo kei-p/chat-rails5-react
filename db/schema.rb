6d62cf0a17787dc037da92de7ffd0f0d4435cf64
Keisuke Ishizawa <muddyapesjm66@gmail.com>
2017-03-19 16:20:46 +0900

    モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319070919) do
+ActiveRecord::Schema.define(version: 20170319071752) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "room_id"
+    t.integer  "user_id"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_comments_on_room_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
