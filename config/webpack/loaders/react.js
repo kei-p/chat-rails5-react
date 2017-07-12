c665e9eb121fe3a24612bc48de3f18aa649c1ebf
Keisuke Ishizawa <muddyapesjm66@gmail.com>
2017-07-12 22:11:39 +0900

    本番環境で hmre が有効化されていたのを対応

--- config/webpack/loaders/react.js
+++ config/webpack/loaders/react.js
@@ -1,8 +1,15 @@
-module.exports = {
+const { env } = require('process')
+
+let settings = {
   test: /\.(js|jsx)?(\.erb)?$/,
   exclude: /node_modules/,
   loader: 'babel-loader',
-  options: {
+}
+
+if ( env.NODE_ENV == 'development' ) {
+  settings.options =  {
     presets: ['react-hmre']
   }
 }
+
+module.exports = settings;
